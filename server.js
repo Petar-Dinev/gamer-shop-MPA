f597145a3f1b7652f464092ea352958b6f896857
Petar Dinev <p.dinev55@gmail.com>
2025-05-25 22:26:07 +0300

    Add express config to project

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const expressConfig = require('./configs/expressConfig');
 
-const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+startServer();
+async function startServer() {
+  const app = express();
+  expressConfig(app);
+  app.get('/', (req, res) => {
     res.send('Hello, World!');
-});
+  });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+}

399ef656946d8271f2c3d0b2a94643a75a099ea2
Petar Dinev <p.dinev55@gmail.com>
2025-05-27 10:17:11 +0300

    Add cookie parser library to project

--- configs/expressConfig.js
+++ configs/expressConfig.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const session = require('./middlewares/session');
 const trim = require('../middlewares/trim');
 const hbs = require('express-handlebars').create({
@@ -9,6 +10,7 @@ module.exports = (app) => {
     app.engine('.hbs', hbs.engine);
     app.set('view engine', '.hbs');
     app.use('/static', express.static('static'));
+    app.use(cookieParser());
     app.use(express.urlencoded({ extended: true }));
     app.use(trim());
     app.use(session());

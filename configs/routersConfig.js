a80ff9b387de83492a4b46fa20ae1bda461c2283
Petar Dinev <p.dinev55@gmail.com>
2025-05-31 18:30:44 +0300

    Add game item partial, refactor navigation and connect game router to main router

--- configs/routersConfig.js
+++ configs/routersConfig.js
@@ -1,7 +1,9 @@
 const authController = require('../controllers/authController');
 const homeController = require('../controllers/homeController');
+const gamesController = require('../controllers/gamesController');
 
 module.exports = (app) => {
     app.use('/', homeController);
     app.use('/auth', authController)
+    app.use('/games', gamesController);
 }
